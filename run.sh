47c7197c7635e0aa8806cf7b5bf3d38e480dfb4c
Minimum <s5550055@yahoo.com.tw>
2015-10-19 16:57:45 +0800

    Change status checking.

--- run.sh
+++ run.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 WAIT_SERVICE_READY=10
+pidfile=var/app.pid
 
-function check_service(){
-  status=$($WORKDIR/control status)
-  echo $status | grep -q "stoped"
-  if [ $? -eq 0 ] ; then
-    return 1
-  else
+function check_pid(){
+    if [ -f $pidfile ];then
+        pid=`cat $pidfile`
+        if [ -n $pid ]; then
+            running=`ps -p $pid|grep -v "PID TTY" |wc -l`
+            return $running
+        fi
+    fi
     return 0
-  fi
 }
 
 tar -zxf $PACKDIR/$PACKFILE -C $WORKDIR
 cp $CONFIGDIR/$CONFIGFILE $WORKDIR
 $WORKDIR/control restart
 sleep $WAIT_SERVICE_READY
-check_service
-if [ $? -eq 0 ] ; then
+check_pid
+if [ $? -gt 0 ] ; then
   $WORKDIR/control tail
 else
   echo "Failed to start."
